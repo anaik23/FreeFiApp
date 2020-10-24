b51dc0ebe6d5634ae81acd3c5b1e4a7b3cbc2da9
Abhay Naik <abhay.naik@hotmail.com>
2020-10-23 19:25:07 -0700

    Initial Commit

---
+++ FreeFi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreeFi
+//
+//  Created by Abhay Naik on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
