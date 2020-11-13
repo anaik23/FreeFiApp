88832088c53239cc92cf8da50b5d1713c553d5bc
Abhay Naik <abhay.naik@hotmail.com>
2020-11-12 17:35:04 -0800

    Added Design for Expense Screen

---
+++ FreeFi/NewExpenseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewExpenseViewController.swift
+//  FreeFi
+//
+//  Created by Abhay Naik on 11/12/20.
+//
+
+import UIKit
+
+class NewExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
